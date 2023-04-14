cd703ca8e355099eeaa928803f4e3de93b061abf
ElysiumStorm <44522892+ElysianStorm@users.noreply.github.com>
2023-04-15 05:21:19 +0530

    Commiting DeepSpeech based Trascriber script and Whisper based Trascriber script.

---
+++ Speech2Text/Speech2Text_Whisper.py
@@ -0,0 +1,79 @@
+import pyaudio
+import wave
+import whisper
+import torch
+
+# Initialize the device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class audioFileCreate(object):
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 5
+    filename = ".tempAudio.wav"
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data) 
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    print("File created")
+
+def main():
+    audioFile = audioFileCreate()
+    model = whisper.load_model("tiny")
+    
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audioFile.filename)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    lang = max(probs, key=probs.get)
+    print(f"Detected language: {lang}")
+
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    # print(result.text)
+
+    # translation for other languages
+    print(f"Translated text from {lang} to en")
+    text = model.transcribe(audioFile.filename, task = 'translate')
+    print(text["text"]) 
+    
+if __name__ == '__main__':
+    main()
